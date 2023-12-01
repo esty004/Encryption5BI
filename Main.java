48a1bb69886e937042dadb418c5fd0f16bf256fd
esty004 <152624889+esty004@users.noreply.github.com>
2023-12-01 15:55:10 +0100

    ciao

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("hai mai passato un capodanno in casa senza luci!");
   }
 }
